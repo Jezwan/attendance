ef9b902a72efc755b6723d09d746ff0cb313c704
Jezwan <jezwan002@gmail.com>
2023-11-08 07:21:48 +0530

    first commit

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+function getData(){
+    var input = document.getElementById("search").value
+    console.log(input)
+}
+function getDate(){
+    var date = document.getElementById("date").value
+    console.log(date)
+}
