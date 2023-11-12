8e1168e2a494aaff03c288bceb75c8039be12d23
Jezwan <jezwan002@gmail.com>
2023-11-12 18:57:21 +0530

    UNIX timestamp is fetched from dynamodb, timestamp() is used to convert them into readable format, and hence time and date is addded to the dashboard table. Also new stylings are given to the table

--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,23 @@
 callData()
 
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var name = document.getElementById('name').value;
-    var date = document.getElementById('date').value;
+    event.preventDefault()
+    var name = document.getElementById('name').value
+    var date = document.getElementById('date').value
 
     fetch('https://0m3q5zuw40.execute-api.eu-north-1.amazonaws.com/invoke/dbmanager')
-    .then(res => res.json())
+    .then(res => {
+        return res.json()
+    })
     .then(data => {
         if (date == '') {
-            var object = data.filter(item => item.name.toLowerCase() == name.toLowerCase());
+            var object = data.filter(item => item.name.toLowerCase().replace(/\s/g, '').includes(name.toLowerCase().replace(/\s/g, '')))
         }
         else if (name == '') {
-            var object = data.filter(item => item.date == date);
+            var object = data.filter(item => timestamp(item.ts).date == date)
         }
         else {
-            var object = data.filter(item => item.name.toLowerCase() == name.toLowerCase() && item.date == date);
+            var object = data.filter(item => item.name.toLowerCase().replace(/\s/g, '') == name.toLowerCase().replace(/\s/g, '') && timestamp(item.ts).date == date)
         }
         return object
     })
@@ -26,7 +28,8 @@ document.querySelector('form').addEventListener('submit', function(event) {
                 const payload = `<tr>
                                 <th scope="row">${user.name}</th>
                                 <td>${user.id}</td>
-                                <td>${user.date}</td>
+                                <td>${timestamp(user.ts).date}</td>
+                                <td>${timestamp(user.ts).time}</td>
                                 <td>${user.status}</td>
                                 </tr>`
                 document.getElementById('table').insertAdjacentHTML('beforeend', payload)
@@ -36,27 +39,38 @@ document.querySelector('form').addEventListener('submit', function(event) {
             callData()
         }
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
 
-});
+})
 
 //calling data from aws
 
 function callData() {
     fetch('https://0m3q5zuw40.execute-api.eu-north-1.amazonaws.com/invoke/dbmanager')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            data.forEach(user => {
-                const payload = `<tr>
-                                    <th scope="row">${user.name}</th>
-                                    <td>${user.id}</td>
-                                    <td>${user.date}</td>
-                                    <td>${user.status}</td>
-                                </tr>`
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        data.forEach(user => {
+            const payload = `<tr>
+                            <th scope="row">${user.name}</th>
+                            <td>${user.id}</td>
+                            <td>${timestamp(user.ts).date}</td>
+                            <td>${timestamp(user.ts).time}</td>
+                            <td>${user.status}</td>
+                            </tr>`
                 document.getElementById('table').insertAdjacentHTML('beforeend', payload)
-            });
         })
-        .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error))
+}
+
+//covert UNIX timestamp to ISO
+
+function timestamp(unixTimestamp) {
+    const dateObject = new Date(Number(unixTimestamp))
+    return {
+        date : dateObject.getFullYear().toString()+"-"+(dateObject.getMonth()+1).toString()+"-"+dateObject.getDate().toString(),
+        time : dateObject.getHours().toString()+":"+dateObject.getMinutes().toString()+":"+dateObject.getSeconds().toString()
+    }
 }
